38b9afae29d7f87d3b43585eff41485f7e9b3628
arjunambekar-09 <82198977+arjunambekar-09@users.noreply.github.com>
2022-06-18 12:39:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+avengers = "";
+parates = "";
+
+function preload(){
+    pirates = loadSound("Pirates.mp3");
+    avengers = loadSound("Avengers.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500)
+}
